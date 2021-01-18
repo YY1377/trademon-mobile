9bb8491a7f7dc85eb94a8bc4cad4518df26d2229
hakansel <hakansel05@gmail.com>
2021-01-18 11:07:06 +0300

    ui refactor.

--- lib/main.dart
+++ lib/main.dart
@@ -35,28 +35,17 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  var totalTransaction = 0;
-  var savingCapital = "-";
-  var currentCapital = "-";
-  var currentStatus = "-";
+  Summary summary;
 
   Map<String, List<CurrencyRate>> providerCurrencyRateMap = new Map();
 
   Future<void> summarize() async {
     var response = await http.get('http://localhost:3333/status/summarize');
     if (response.statusCode == 200) {
-      var transaction = Summary.fromRawJson(response.body.toString());
-      var savingCapital = transaction.savingCapital;
-      var currentCapital = transaction.currentCapital;
-      var currentStatus = transaction.currentStatus;
-      var diffAmount = currentStatus.diffAmount;
-      var diffPercentage = currentStatus.diffPercentage;
-      var diff = currentStatus.diff;
+      var summary = Summary.fromRawJson(response.body.toString());
       setState(() {
-        this.totalTransaction = transaction.totalTransaction;
-        this.savingCapital = '$savingCapital ₺';
-        this.currentCapital = '$currentCapital  ₺';
-        this.currentStatus = '$diffAmount ₺ (% $diffPercentage) $diff';
+        // ₺
+        this.summary = summary;
       });
     }
   }
@@ -98,71 +87,7 @@ class _MyHomePageState extends State<MyHomePage> {
               'Status',
               style: Theme.of(context).textTheme.headline3,
             ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Spacer(),
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      'Total Transaction',
-                      textScaleFactor: 0.8,
-                      style: TextStyle(fontWeight: FontWeight.w300),
-                    ),
-                    Text(
-                      '$totalTransaction',
-                      style: TextStyle(fontSize: 14, fontWeight: FontWeight.w700),
-                    ),
-                  ],
-                ),
-                Spacer(),
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      'Saving Capital',
-                      textScaleFactor: 0.8,
-                      style: TextStyle(fontWeight: FontWeight.w300),
-                    ),
-                    Text(
-                      '$savingCapital',
-                      style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
-                    ),
-                    Text(
-                      'Current Capital',
-                      textScaleFactor: 0.8,
-                      style: TextStyle(fontWeight: FontWeight.w300),
-                    ),
-                    Text(
-                      '$currentCapital',
-                      style: TextStyle(fontSize: 18, fontWeight: FontWeight.w700),
-                    ),
-                  ],
-                ),
-                Spacer(),
-                Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-                  Text(
-                    'Current Status',
-                    textScaleFactor: 0.8,
-                    style: TextStyle(fontWeight: FontWeight.w300),
-                  ),
-                  Text(
-                    '$currentStatus'.split("\ ")[0] + ' ' + '$currentStatus'.split("\ ")[1],
-                    style: TextStyle(fontSize: 14, fontWeight: FontWeight.w700),
-                  ),
-                  Text(
-                    '$currentStatus'.split("\ ")[2] + ' ' + '$currentStatus'.split("\ ")[3],
-                    style: TextStyle(fontSize: 14, fontWeight: FontWeight.w700),
-                  ),
-                  Text(
-                    '$currentStatus'.split("\ ")[4],
-                    style: TextStyle(fontSize: 14, fontWeight: FontWeight.w700),
-                  ),
-                ]),
-                Spacer(),
-              ],
-            ),
+            getCurrentStatusRow(summary),
             Spacer(
               flex: 2,
             ),
@@ -182,6 +107,92 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Icon(Icons.refresh_outlined),
         backgroundColor: Colors.lightGreen,
       ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+  Row getCurrentStatusRow(Summary summary) {
+    if (summary == null) {
+      return Row(
+        children: [Text('-')],
+      );
+    }
+
+    var totalTransaction = summary.totalTransaction ?? '-';
+    var savingCapital = summary.savingCapital + ' ₺' ?? '-';
+    var currentCapital = summary.currentCapital + ' ₺' ?? '-';
+    var currentStatus = summary.currentStatus;
+    var diffAmount = '-';
+    var diffPercentage = '-';
+    var diff = '-';
+    if (currentStatus != null) {
+      diffAmount = currentStatus.diffAmount + ' ₺' ?? '-';
+      diffPercentage = ' %' + (currentStatus.diffPercentage ?? '-').toString();
+      diff = currentStatus.diff ?? '-';
+    }
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Spacer(),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Total Transaction',
+              textScaleFactor: 0.8,
+              style: TextStyle(fontWeight: FontWeight.w300),
+            ),
+            Text(
+              '$totalTransaction',
+              style: TextStyle(fontSize: 14, fontWeight: FontWeight.w700),
+            ),
+          ],
+        ),
+        Spacer(),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Saving Capital',
+              textScaleFactor: 0.8,
+              style: TextStyle(fontWeight: FontWeight.w300),
+            ),
+            Text(
+              '$savingCapital',
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
+            ),
+            Text(
+              'Current Capital',
+              textScaleFactor: 0.8,
+              style: TextStyle(fontWeight: FontWeight.w300),
+            ),
+            Text(
+              '$currentCapital',
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.w700),
+            ),
+          ],
+        ),
+        Spacer(),
+        Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+          Text(
+            'Current Status',
+            textScaleFactor: 0.8,
+            style: TextStyle(fontWeight: FontWeight.w300),
+          ),
+          Text(
+            '$diffAmount',
+            style: TextStyle(fontSize: 14, fontWeight: FontWeight.w700),
+          ),
+          Text(
+            '$diffPercentage',
+            style: TextStyle(fontSize: 14, fontWeight: FontWeight.w700),
+          ),
+          Text(
+            '$diff',
+            style: TextStyle(fontSize: 14, fontWeight: FontWeight.w700),
+          ),
+        ]),
+        Spacer(),
+      ],
     );
   }
 
